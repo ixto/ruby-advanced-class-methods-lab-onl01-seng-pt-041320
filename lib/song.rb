027524cd333d98394cf8d21199d54c125faedf03
Joey <joey.alzamora@gmail.com>
2020-05-14 23:43:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,19 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new  
+    @@all << song
+    song 
+  end 
+  
+  def self.new_by_name
+    @new_by_name = new_by_name 
+  end 
 end
+
+# def self.create
+#     song = self.new
+#     @@all << song
+#     song
+#   end
